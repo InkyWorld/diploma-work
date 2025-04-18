1ab05814c018ecdbaed721dcb54ca7387451a842
ink404dot <94466285+ink404dot@users.noreply.github.com>
2025-04-18 11:06:48 +0300

    user model

---
+++ backend/app/models/models.py
@@ -0,0 +1,37 @@
+import enum
+from datetime import datetime, date
+
+from sqlalchemy import Boolean, String, Integer, DateTime, ForeignKey, Enum, Date, func
+from sqlalchemy.orm import relationship, mapped_column, Mapped, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Role(enum.Enum):
+    admin = "admin"
+    engineer = "engineer"
+    flight_dispatcher = "flight dispatcher"
+    shift_supervisor = "shift supervisor"
+    technician = "technician"
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    img_profile: Mapped[str] = mapped_column(String(2000), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column(
+        "updated_at", DateTime, default=func.now(), onupdate=func.now()
+    )
+    role: Mapped[Enum] = mapped_column(
+        "role", Enum(Role), nullable=False
+    )
+
+
