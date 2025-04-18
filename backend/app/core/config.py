d4adf62fe1fd994049bfe8689488ad79bf6e54e1
ink404dot <94466285+ink404dot@users.noreply.github.com>
2025-04-18 11:39:30 +0300

    redis connection

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -18,5 +18,19 @@ class Settings(BaseSettings):
 class DBConfig(Settings):
     DATABASE_URL: str = "postgresql+asyncpg://postgres:111111@localhost:5432/abc"
 
+class JWTConfig(Settings):
+    SECRET_KEY: str = "1234567890"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
+class RedisConfig(Settings):
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_PASSWORD: Optional[str] = None
+    REDIS_DB: int = 0
+
+
+config_redis = RedisConfig()
 db_config = DBConfig()
+jwt_config = JWTConfig()
