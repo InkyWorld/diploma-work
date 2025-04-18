f08f285030bed42760f23499854a75182cd09bbc
ink404dot <94466285+ink404dot@users.noreply.github.com>
2025-04-18 11:30:20 +0300

    create user schema

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,23 @@
+from abc import ABC
+from typing import Annotated
+
+from pydantic import BaseModel, Field, EmailStr
+
+from app.models.models import Role
+
+class UserResponseSchema(BaseModel):
+    pass
+
+
+class UserCreationSchema(BaseModel):
+    password: Annotated[str, Field(min_length=6, max_length=255)]
+    full_name: Annotated[str, Field(min_length=6, max_length=255)]
+    email: EmailStr
+    role: Role
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
