49e8ec10d3cf9891ad05b952341c2e634cc77a25
ink404dot <94466285+ink404dot@users.noreply.github.com>
2025-04-22 16:29:15 +0300

    refactor api structure

--- backend/app/api/roles/admin.py
+++ backend/app/api/roles/admin.py
@@ -93,7 +93,7 @@ async def get_all_users(
         )
     return users
 
-@admin_router.delete("/user", response_model=List[UserResponseSchema], dependencies=[Depends(admin_only_access)], status_code=status.HTTP_200_OK)
+@admin_router.delete("/user", dependencies=[Depends(admin_only_access)], status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(
     request: Request,
     email: EmailStr = Query(..., description="Email of the user to delete"),
@@ -101,9 +101,70 @@ async def delete_user(
 ):
     user = await repositories_users.get_user_by_email(email, db)
     if not user:
-        raise HTTPException(status_code=404, detail=f"User with ID {id} not found")
+        raise HTTPException(status_code=404, detail=f"User with email {email} not found")
 
     await repositories_users.delete_user(email, db)
+    await claudinary.delete_avatar_from_cloudinary(email)
+    return
+
+@admin_router.patch("/user", response_model=UserResponseSchema, dependencies=[Depends(admin_only_access)], status_code=status.HTTP_200_OK)
+async def edit_user(
+            bt: BackgroundTasks, 
+            request: Request,
+            email: EmailStr = Query(..., description="Email of the user to update"),
+            password: str = Form(None),
+            full_name: str = Form(None),
+            role: str = Form(None),
+            age: int = Form(None),
+            gender: str = Form(None),
+            img_profile: UploadFile = File(None),
+            db: AsyncSession = Depends(get_db)
+        ):
+    user = await repositories_users.get_user_by_email(email, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     
-    users = await repositories_users.get_all_users_from_db(db)
-    return users
+    if img_profile:
+        max_size = 10 * 1024 * 1024  # 5 MB max file size
+        allowed_extensions = ['.jpg', '.jpeg', '.png']
+        if not any(img_profile.filename.endswith(ext) for ext in allowed_extensions):
+            raise HTTPException(status_code=400, detail="Invalid file type. Allowed formats are: jpg, jpeg, png.")
+        
+        if img_profile.size == 0:
+            raise HTTPException(status_code=400, detail="File is empty.")
+
+        # Read file in chunks
+        current_size = 0
+        chunk_size = 1024 * 1024
+        while True:
+            chunk = await img_profile.read(chunk_size)
+            if not chunk:  # No more data to read
+                break
+            current_size += len(chunk)
+            
+            # Check if the file size exceeds the limit
+            if current_size > max_size:
+                raise HTTPException(status_code=400, detail="File size exceeds the allowed limit of 5MB.")
+        img_profile.file.seek(0)
+        await claudinary.delete_avatar_from_cloudinary(email)
+        img_profile = await claudinary.upload_avatar_to_cloudinary(img_profile, email)
+    if password:
+        password = auth_service.get_password_hash(password)
+
+    update_data = {
+        "full_name": full_name,
+        "password": password,
+        "role": role,
+        "age": age,
+        "gender": gender,
+        "img_profile": img_profile,
+    }
+
+    # Filter out None values
+    update_data = {k: v for k, v in update_data.items() if v is not None}
+
+    if not update_data:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No fields provided for update")
+
+    updated_user = await repositories_users.update_user(email, update_data, db)
+    return updated_user
